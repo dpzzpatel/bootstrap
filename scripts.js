6be08fd49e1f39c78f7d587265dec53e020706df
dpzzpatel <dpzzpatel@gmail.com>
2019-11-14 22:26:57 +0530

    redirecting

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function AboutUs()
+{
+    window.location.href = "./AboutUs.html";
+}
+function Home()
+{
+  	window.location.href = "./home.html";
+}
+function Category()
+{
+	window.location.href = "./category.html";
+}
